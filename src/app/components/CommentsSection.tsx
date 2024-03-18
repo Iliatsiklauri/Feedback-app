92597d767b40cf316bed325dc417eba0565dcaf8
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-03-18 18:36:07 +0400

    fixed commentSection component bug

--- src/app/components/CommentsSection.tsx
+++ src/app/components/CommentsSection.tsx
@@ -21,13 +21,13 @@ export default function CommentsSection({ comments, id, ind }: PropType) {
       <header className="flex items-center justify-between w-full">
         <div className="flex gap-6 items-center justify-center">
           <img
-            src={`${comments?.user.image}`}
+            src={`${comments?.user?.image}`}
             alt=""
             className="w-10 h-10 rounded-full"
           />
           <div className="flex items-start justify-center flex-col">
-            <p className="font-bold text-sm text-[#3A4374]">{comments?.user.name}</p>
-            <p className="text-[#647196] text-sm">{comments?.user.username}</p>
+            <p className="font-bold text-sm text-[#3A4374]">{comments?.user?.name}</p>
+            <p className="text-[#647196] text-sm">{comments?.user?.username}</p>
           </div>
         </div>
         <p className="text-[#4661E6] text-sm font-semibold cursor-pointer">Reply</p>
