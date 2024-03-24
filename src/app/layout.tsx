6307906c5d4674e311f2677259626abeb03d63e1
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-03-24 17:56:58 +0400

    starting all over

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Jost } from 'next/font/google';
 import './globals.css';
-import GlobalStates from './GlobalStates';
 
 const jost = Jost({ subsets: ['latin'] });
 
@@ -17,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jost.className}>
-        <GlobalStates>{children}</GlobalStates>
-      </body>
+      <body className={jost.className}>{children}</body>
     </html>
   );
 }
